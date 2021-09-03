dc68868bf57a4ff1fe65cf41598beb6ea146f0cf
razzanm <rajuengir@gmail.com>
2021-09-03 17:29:19 +0530

    first set up

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const app = () => {
+  return(<>
+    <h1>Welcome to React Parcel Micro App!</h1>
+    <p>Hard to get more minimal than this React app.</p>
+    <p>hi my name is rajan</p>
+  </>);
+  
+}
+export default app;
